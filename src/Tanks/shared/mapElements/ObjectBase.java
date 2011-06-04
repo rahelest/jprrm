a37e4cdeb70a4f694d06878b6a686a44c1fe3849
janszap <janszap@9e466fe3-8e9c-b258-af60-af6983a769f2>
2011-06-04 18:39:18 +0000

    JComponendis juba olevad väljad ja meetodid eemaldatud.

--- src/Tanks/shared/mapElements/ObjectBase.java
+++ src/Tanks/shared/mapElements/ObjectBase.java
@@ -3,9 +3,5 @@ package Tanks.shared.mapElements;
 public interface ObjectBase {
 
 	public boolean getCollision(GameObject otherObject);
-	public int getLocationX();
-	public int getLocationY();
-	public int getWidth();
-	public int getHeight();
 	public boolean isBreakable();
 }

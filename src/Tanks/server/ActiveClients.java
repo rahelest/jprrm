ceae3a098052d882ec38fcec2202205ece231511
janszap <janszap@9e466fe3-8e9c-b258-af60-af6983a769f2>
2011-06-06 13:51:05 +0000

    CheckStyled!

--- src/Tanks/server/ActiveClients.java
+++ src/Tanks/server/ActiveClients.java
@@ -1,47 +1,62 @@
 package Tanks.server;
 
 import java.net.InetAddress;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * The class that hold the list of active clients.
+ * @author JPRRM
+ *
+ */
 public class ActiveClients {
 
+	/**
+	 * List with the clients.
+	 */
 	private CopyOnWriteArrayList<ClientSession> clientList = new CopyOnWriteArrayList<ClientSession>();
-	private Object iteratorLock = new Object();
 	
+	/**
+	 * Adds a new client to the list.
+	 * @param client The new client.
+	 */
 	public void addClient(ClientSession client) {
 			clientList.add(client);
 	}
 	
-	
-
+	/**
+	 * Return the iterator of the list.
+	 * @return The iterator.
+	 */
 	public Iterator<ClientSession> iterator() {
 			Iterator<ClientSession> i = clientList.iterator();
 			return i;
 	}
 
+	/**
+	 * Checks whether the clients are present.
+	 * @param inetAddress The IP-address.
+	 * @return Whether the client exists on the list.
+	 */
 	public boolean exists(InetAddress inetAddress) {
-		// TODO Auto-generated method stub
 		for (ClientSession temp : clientList) {
-			if(temp.getClientIP().equals(inetAddress)) {
+			if (temp.getClientIP().equals(inetAddress)) {
 				return true;
 			}
 		}
 		return false;
 		
 	}
-	
+
+	/**
+	 * Asks the list for the existing client.
+	 * @param inetAddress The IP after which to search.
+	 * @return The found client.
+	 */
 	public ClientSession getExisting(InetAddress inetAddress) {
 		ClientSession temp = null;
 		for (ClientSession pointer : clientList) {
-			if(pointer.getClientIP().equals(inetAddress)) {
+			if (pointer.getClientIP().equals(inetAddress)) {
 				temp = pointer;
 				break;
 			}		

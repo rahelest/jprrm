033033afa996109304bed719749da6950fa65565
rahelini <rahelini@9e466fe3-8e9c-b258-af60-af6983a769f2>
2011-11-18 11:31:11 +0000

    komment

--- src/UusPakk/Node.java
+++ src/UusPakk/Node.java
@@ -10,14 +10,38 @@ import java.util.ArrayList;
 
 public class Node {
 	
+	/**
+	 * Tipu kauguse puu juurelemendist.
+	 */
 	private int depth;
+	/**
+	 * Tipule omistatud väärtus.
+	 */
 	private int value;
+	/**
+	 * Tipule omistatud kaal.
+	 */
 	private int weight;
+	/**
+	 * Ahne algoritmi ennustus sellele tipule, teatud juhtudel ka väärtuse/kaalu suhe..
+	 */
 	private float bound;
+	/**
+	 * Väärtuse / kaalu suhe.
+	 */
 	private float ratio;
+	/**
+	 * Järgmise elemendi viide pinu jaoks.
+	 */
 	private Node next;
+	/**
+	 * Nimekiri valitud elementidest.
+	 */
 	private ArrayList<Boolean> valikud;
 	
+	/**
+	 * Konstruktor.
+	 */
 	public Node() {
 		depth = -1;
 		value = 0;
@@ -26,6 +50,12 @@ public class Node {
 		ratio = 0;
 	}
 	
+	/**
+	 * Konstruktor koos mainitud väljade väärtustamisega.
+	 * @param newDepth Tipu sügavus.
+	 * @param newValue Tipu väärtus.
+	 * @param newWeight Tipu kaal.
+	 */
 	public Node (int newDepth, int newValue, int newWeight) {
 		depth = newDepth;
 		value = newValue;
@@ -39,97 +69,139 @@ public class Node {
 	}
 	
 	/**
-	 * @return the ratio
+	 * Väärtuse / kaalu suhte küsimine.
+	 * @return Väärtuse / kaalu suhe.
 	 */
 	public float getRatio() {
 		return ratio;
 	}
 
+	/**
+	 * Konstruktor pinu jaoks määrab ainult väärtuse ja järgmise elemendi viite.
+	 * @param val Tipu väärtus.
+	 * @param n Järgmise elemendi viide.
+	 */
 	public Node(int val, Node n) {
 		value = val;
 	}
 	
+	/**
+	 * Elemendi väärtuse küsimine.
+	 * @return Elemendi väärtus.
+	 */
 	public int get() {
 		return value;
 	}
 
 	/**
-	 * @return the depth
+	 * Elemendi sügavuse küsimine.
+	 * @return Elemendi sügavus.
 	 */
 	public int getDepth() {
 		return depth;
 	}
 
 	/**
-	 * @param depth the depth to set
+	 * Elemendi sügavuse määramine.
+	 * @param Elemendi uus sügavus.
 	 */
 	public void setDepth(int depth) {
 		this.depth = depth;
 	}
 
 	/**
-	 * @return the value
+	 * Elemendi väärtuse küsimine.
+	 * @return Elemendi väärtus.
 	 */
 	public int getValue() {
 		return value;
 	}
 
 	/**
-	 * @param value the value to set
+	 * Elemendi väärtuse määramine.
+	 * @param Määratav väärtus.
 	 */
 	public void setValue(int value) {
 		this.value = value;
 	}
 
 	/**
-	 * @return the weight
+	 * Elemendi kaalu küsimine.
+	 * @return Elemendi kaal.
 	 */
 	public int getWeight() {
 		return weight;
 	}
 
 	/**
-	 * @param weight the weight to set
+	 * Elemendi kaalu määramine.
+	 * @param weight Elemendi uus kaal.
 	 */
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
 
 	/**
-	 * @return the bound
+	 * Elemendi tulemushinnangu küsimine.
+	 * @return the bound Elemendi tulemushinnang.
 	 */
 	public float getBound() {
 		return bound;
 	}
 
 	/**
-	 * @param bound the bound to set
+	 * Elemendi tulemushinnangu määramine.
+	 * @return the bound Elemendi tulemushinnang.
 	 */
 	public void setBound(float bound) {
 		this.bound = bound;
 	}
 	
+	/**
+	 * Järgmise elemendi küsimine.
+	 * @return Järgmine element.
+	 */
 	public Node getNext() {
 		return next;
 	}
 	
+	/**
+	 * Järgmise elemendi määramine.
+	 * @return Määratav element.
+	 */
 	public void setNext (Node n) {
 		next = n;
 	}
 	
+	/**
+	 * Stringesitus paremaks loetavuseks.
+	 * @return Kirjeldus.
+	 */
 	public String toString() {
 //		return "\nNode - Väärtus: " + value + " Kaal: " + weight;
 		return "( " + value + ", " + weight + ", " + ratio + " ) ";
 	}
 
+	/**
+	 * Valikute nimekirja küsimine.
+	 * @param valikud Valikute nimekiri.
+	 */
 	public ArrayList<Boolean> getValikud() {
 		return valikud;
 	}
 
+	/**
+	 * Valikute nimekirja määramine.
+	 * @param valikud Uus valikute nimekiri.
+	 */
 	public void setValikud(ArrayList<Boolean> valikud) {
 		this.valikud = valikud;
 	}
 
+	/**
+	 * Valikute nimekirjast kasutute elementide eemaldamine (false-d lõpus).
+	 * @return Uus ja parem node.
+	 */
 	public Node trimZeros() {
 		for (int i = valikud.size() - 1; i >= 0; i++) {
 			if (valikud.get(i)) {

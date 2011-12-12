98f24fddac6e13171c2faab7a196fd8366e279fd
janszap <janszap@9e466fe3-8e9c-b258-af60-af6983a769f2>
2011-12-12 04:19:50 +0000

    Toimus Maze ja IO unifitseerimine

--- trunk/src/labyrinth/Maze.java
+++ trunk/src/labyrinth/Maze.java
@@ -1,6 +1,13 @@
 package labyrinth;
 
 import java.awt.Dimension;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
@@ -19,7 +26,7 @@ public class Maze {
 	/**
 	 * Labürindi sissepääsu koordinaadid. -legacy (failist lugemine)
 	 */
-	Dimension entrance;
+	static Dimension entrance;
 	
 	/**
 	 * Asukoht, mis on põhimõtteliselt ruuduke labürindis, sissepääs
@@ -29,7 +36,7 @@ public class Maze {
 	/**
 	 * Labürindi väljapääsu koordinaadid. -legacy (failist lugemine)
 	 */
-	Dimension exit;
+	static Dimension exit;
 	
 	/**
 	 * Asukoht, mis on põhimõtteliselt ruuduke labürindis, väljapääs
@@ -580,6 +587,112 @@ public class Maze {
 				return 1;
 			}
 		}
+		
+	}
+	
+	/*
+	 * FAILI LUGEMISE MEETODID
+	 */
+	
+	/**
+	 * Faili nimi, millest hakatakse labürinti lugema
+	 * ja kuhu tulemus kirjutatakse.
+	 */
+	static String filename;
+	
+	/**
+	 * Main meetod.
+	 * @param args Faili nimi.
+	 */
+	public static void main(String[] args) {
+		filename = args[0];
+		char[][] maze = readInput(filename);
+		Maze m = new Maze(entrance, exit);
+//		m.printOut(maze);
+		maze = m.solve(maze);
+		m.printOut(maze);
+		writeOutput(maze);	
+	}
+	
+	/**
+	 * Faili info sisselugeja.
+	 * @param string Faili nimi, kust loetakse.
+	 * @return Labürint, mida töötlema hakatakse.
+	 */
+	private static char[][] readInput(String string) {
+		BufferedReader br;
+		char [][] maze = null;
+		int n = 0;
+		try {
+			br = new BufferedReader(
+						new InputStreamReader(new FileInputStream("in\\" + 
+									string + ".in")));
+			
+			/**
+			 * Esimene rida näitab ära labürindi suuruse,
+			 * see loetakse sisse eraldi. Vastavalt sellele
+			 * luuakse ka 2n+1 dimensioonidega char maatriks.
+			 */
+			n = Integer.parseInt(br.readLine());
+			maze = new char[2 * n + 1][2 * n + 1];
+
+			String line = br.readLine();
+			int f = 0;
+			while (line != null) {
+				for (int i = 0; i < maze.length; i++) {
+					maze[f][i] = line.charAt(i);
+
+					/**
+					 * Kontrollib, kas loetud täht on B või F,
+					 * sel juhul kirjutatakse koordinaadid 
+					 * vastavalt entrance või exit muutujasse.
+					 */
+					if (line.charAt(i) == 'B') {
+						entrance = new Dimension(f, i);
+					} else if (line.charAt(i) == 'F') {
+						exit = new Dimension(f, i);
+					}
+				}
+
+				f++;
+				line = br.readLine();
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Faili ei leitud! Kontrolli sisendparameetrit.");
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (NumberFormatException e) {
+			System.out.println("Esimene rida failis ei ole number!");
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return maze;
+	}
+
+	/**
+	 * Kirjutab labürindi muutuja faili.
+	 * @param maze Uus labürint.
+	 */
+	private static void writeOutput(char[][] maze) {
+		FileWriter outputStream;
+		BufferedWriter out;
+		try {
+			outputStream = new FileWriter("out\\" +
+					filename + ".out");
+			out = new BufferedWriter(outputStream);
+			for (char[] row : maze) {
+				out.write(row);
+				out.write("\n");
+			}			
+			out.close();
+		} catch (IOException e) {
+			System.out.println("Programm läks katki, kahju küll.");
+			e.printStackTrace();
+		}		  
 		
 	}
 }

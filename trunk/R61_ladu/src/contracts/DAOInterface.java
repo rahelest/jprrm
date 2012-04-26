fb9a4c75fc7ac26d0e82662a84f4ba5fa956ef16
rahelini <rahelini@9e466fe3-8e9c-b258-af60-af6983a769f2>
2012-04-26 12:10:42 +0000

    Tegin algust uue projektiga õigesti, enne ei olnud "dynamic web project"

---
+++ trunk/R61_ladu/src/contracts/DAOInterface.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package contracts;
+
+/**
+ * @author Rahel
+ * Selle asemel peame mby kasutama seda automaatset asja nimega "hibernate"
+ */
+public interface DAOInterface {
+
+}
---
+++ trunk/R61_ladu/src/contracts/FacadeInterface.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package contracts;
+
+/**
+ * @author Rahel Fassaad t�mbab k�ima backendi erinevaid klasse.
+ */
+public interface FacadeInterface {
+
+}
